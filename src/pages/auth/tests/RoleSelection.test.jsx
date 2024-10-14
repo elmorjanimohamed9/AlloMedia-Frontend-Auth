a5c1e683418944113492a676ce4d221c8267a571
Mohamed El Morjani <elmorjanimohamed9@gmail.com>
2024-10-14 14:52:35 +0100

    fixed logout

--- src/pages/auth/tests/RoleSelection.test.jsx
+++ src/pages/auth/tests/RoleSelection.test.jsx
@@ -11,14 +11,11 @@ describe('RoleSelection Component', () => {
       </MemoryRouter>
     );
 
-    // Select the "client" role
     const clientRadio = screen.getByLabelText(/I'm a client, hiring for a project/i);
     fireEvent.click(clientRadio);
 
-    // Check if the client role is selected
     expect(clientRadio.checked).toBe(true);
 
-    // Check if the submit button is enabled
     const submitButton = screen.getByRole('button', { name: /create account/i });
     expect(submitButton).not.toBeDisabled();
 
